5c7c21e32858ca8bb47590ac6e80cde3131b2432
Greyflanel <richardlovelace811@gmail.com>
2019-04-18 17:57:56 -0500

    My fourth commit. Still working.

--- instagram/instagram-app/src/components/CommentSection.js
+++ instagram/instagram-app/src/components/CommentSection.js
@@ -1,39 +1,23 @@
 import React from "react";
-
 import Comment from "./Comment";
 import PropTypes from "prop-types";
 
-class CommentSection extends React.Component {
-   constructor(props) {
-     super(props);
-    //  console.log(props);
-    this.state = {
-        usernameComments: "Richard",
-        inputText: ""
-  }
-}
-
-updateComments = () => {
-  this.setState({
-    inputText: this.state.inputText
-  })
-}
-  render() {
+const CommentSection = props => {
+  // console.log(props);
   return (
     <div>
-      {this.props.comment.map((comments, index) => (
+      {props.comment.map((comments, index) => (
         <Comment key={index} text={comments.text} user={comments.username} />
       ))}
-      <input id="commentField" type="text"
-      value={this.state.inputText}
-             placeholder="Comment here" onChange={event => this.setState({
-               inputText: event.target.value
-             })} />
-      <button onClick={this.state.updateComments}>Add Comment</button>
+      {/* <input type="text" onChange={event => 
+      this.setState({
+        inputText: event.target.value
+      })} value={props.comments}/>
+      <button>Add Comment</button> */}
     </div>
   );
 };
-}
+
 CommentSection.propTypes = {
   comment: PropTypes.arrayOf(PropTypes.object),
  
